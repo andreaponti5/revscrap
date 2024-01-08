3cf2b61696d48baddf94685621dd9558a1334ae4
Andrea Ponti <andrea.ponti@unimib.it>
2024-01-08 16:17:11 +0100

    First commit

---
+++ app.py
@@ -0,0 +1,68 @@
+import dash_bootstrap_components as dbc
+from dash import Dash, html, Input, Output, State, dcc
+from dash.exceptions import PreventUpdate
+
+from scraper import get_app_id_name_from_appstore_url, retrieve_appstore_reviews, formate_appstore_reviews, \
+    get_app_id_from_playstore_url, retrieve_playstore_reviews, format_playstore_reviews
+
+
+app = Dash(__name__,
+           external_stylesheets=[
+               dbc.icons.BOOTSTRAP,
+               dbc.themes.BOOTSTRAP,
+           ])
+
+app.layout = html.Div([
+    html.H1("REVIEW SCRAPER", style={"text-align": "center", 'margin-top': '1%'}),
+    dbc.Col([
+        dbc.Row([
+            dbc.Input(
+                placeholder="Enter app url...",
+                size="lg",
+                id='input-url',
+                style={'width': '70%', 'margin-right': '1%'}
+            ),
+            dbc.Button([
+                html.I(className='bi bi-search', style={'font-size': '30px'}),
+            ],
+                id='search-button',
+                type='button',
+                style={'width': '5%', 'margin-right': '1%'}
+            ),
+        ], justify="center"),
+    ], style={'margin-top': '1%', 'margin-bottom': '1%'}),
+    dcc.Download(id="download-data"),
+    dcc.Interval(id='interval-log', interval=1000, n_intervals=0),
+])
+
+
+@app.callback(
+    Output('download-data', 'data'),
+    [Input('search-button', 'n_clicks'),
+     Input('input-url', 'n_submit')],
+    State('input-url', 'value'),
+    prevent_initial_call=True
+)
+def start_review_scraping(n_clicks, n_submits, url):
+    if n_clicks == 0 and n_submits == 0:
+        raise PreventUpdate
+    if "apps.apple.com" in url:
+        app_id, app_name = get_app_id_name_from_appstore_url(url)
+        appstore_reviews = retrieve_appstore_reviews(app_name=app_name, app_id=app_id)
+        appstore_dataset = formate_appstore_reviews(appstore_reviews)
+        return dcc.send_data_frame(appstore_dataset.to_csv,
+                                   filename=f"appstore_{app_name}_reviews.csv",
+                                   index=False)
+    elif "play.google.com" in url:
+        app_id = get_app_id_from_playstore_url(url)
+        playstore_reviews = retrieve_playstore_reviews(app_id=app_id)
+        playstore_dataset = format_playstore_reviews(playstore_reviews)
+        return dcc.send_data_frame(playstore_dataset.to_csv,
+                                   filename=f"playstore_{app_id.replace('.', '_')}_reviews.csv",
+                                   index=False)
+    else:
+        raise ValueError("Invalid url. Make sure to use a Playstore or Appstore url.")
+
+
+if __name__ == '__main__':
+    app.run(debug=False)
---
+++ scraper.py
@@ -0,0 +1,187 @@
+import sys
+from datetime import datetime
+from typing import Tuple
+
+import pandas as pd
+from app_store_scraper import AppStore
+from google_play_scraper import Sort, reviews, reviews_all
+
+
+def get_app_id_name_from_appstore_url(
+        url: str
+) -> Tuple[str, str]:
+    """
+    Extract the app id and name from an App Store url.
+
+    :param url: the App Store url (e.g., https://apps.apple.com/it/app/enel-x-way/id1377291789)
+    :return: the app id and the app name.
+    """
+    return url.split("/")[-1].replace("id", ""), url.split("/")[-2]
+
+
+def get_app_id_from_playstore_url(
+        url: str
+) -> str:
+    """
+    Extract the app id from a Play Store url.
+
+    :param url: the Play Store url (e.g.,
+        https://play.google.com/store/apps/details?id=com.enel.mobile.recharge2&hl=it&gl=US)
+    :return: the app id.
+    """
+    return url.split("/")[-1].split("?")[-1].split("&")[0].replace("id=", "")
+
+
+def retrieve_appstore_reviews(
+        app_name: str,
+        app_id: str,
+        country: str = "it",
+        how_many: int = sys.maxsize,
+) -> list[dict]:
+    """
+    Retrieve a list of reviews for a given app from the App Store.
+
+    :param app_name: the name of the app. You can find it in the App Store url.
+        E.g., given the url https://apps.apple.com/it/app/enel-x-way/id1377291789, the app name would be "enel-x-way"
+    :param app_id: the id of the app. You can find it in the App Store url.
+        E.g., given the url https://apps.apple.com/it/app/enel-x-way/id1377291789, the app id would be "1377291789"
+    :param country: the considered country to retrieve the reviews.
+    :param how_many: the number of reviews to retrieve. By default, all the reviews will be returned.
+    :return: a list of dictionaries containing reviews.
+        Each review has the following parameter:
+            - 'date': datetime of the review.
+            - 'developerResponse': dictionary containing the id, body and date of the response.
+            - 'review': the text of the review.
+            - 'rating': the number of stars of the review (1-5).
+            - 'isEdited': boolean indicating whether the review has been modified or not.
+            - 'title': the title of the review.
+            - 'userName': the user that created the review.
+    """
+    app_store = AppStore(country=country, app_name=app_name, app_id=app_id)
+    app_store.review(how_many=how_many)
+    return app_store.reviews[:how_many]
+
+
+def retrieve_playstore_reviews(
+        app_id: str,
+        lang: str = 'it',
+        country: str = "it",
+        how_many: int = None,
+) -> list[dict]:
+    """
+    Retrieve a list of reviews for a given app from the Google Play Store.
+
+    :param app_id: the id of the app. You can find it in the Play Store url.
+        E.g., given the url https://play.google.com/store/apps/details?id=com.enel.mobile.recharge2&hl=it&gl=US,
+        the app id would be "com.enel.mobile.recharge2"
+    :param lang: the considered language to retrieve the reviews.
+    :param country: the considered country to retrieve the reviews.
+    :param how_many: the number of reviews to retrieve. If None, all the reviews will be returned.
+    :return: a list of dictionaries containing reviews.
+        Each review has the following parameter:
+            - 'reviewId': the id of the review.
+            - 'userName': the user that created the review.
+            - 'userImage': the image url of the user.
+            - 'content': the text of the review.
+            - 'score': the number of stars of the review (1-5).
+            - 'thumbsUpCount': the number of thumbs up.
+            - 'reviewCreatedVersion': the version of the app considered in the review.
+            - 'at': datetime of the review.
+            - 'replyContent': the response to the review.
+            - 'repliedAt': datetime of the response.
+            - 'appVersion': the current version of the app.
+    """
+    if how_many is None:
+        # Retrieve all the reviews
+        result = reviews_all(
+            app_id=app_id,
+            lang=lang,
+            country=country,
+            sort=Sort.NEWEST,
+        )
+    else:
+        # Retrieve max 200 reviews per request to avoid problems.
+        # 200 is the maximum number of reviews displayed in a page
+        result, continuation_token = [], None
+        count_per_round = 200
+        for i in range(how_many // count_per_round + 1):
+            round_res, continuation_token = reviews(
+                app_id=app_id,
+                lang=lang,
+                country=country,
+                sort=Sort.NEWEST,
+                count=count_per_round,
+                continuation_token=continuation_token
+            )
+            result.extend(round_res)
+    return result[:how_many]
+
+
+def formate_appstore_reviews(
+        revs: list[dict]
+) -> pd.DataFrame:
+    """
+    Format the reviews obtained calling `retrieve_appstore_reviews` in a pandas dataframe.
+
+    :param revs: a list of dictionaries containing reviews from the App Store.
+        Each review has the following parameter:
+            - 'date': datetime of the review.
+            - 'developerResponse': dictionary containing the id, body and date of the response.
+            - 'review': the text of the review.
+            - 'rating': the number of stars of the review (1-5).
+            - 'isEdited': boolean indicating whether the review has been modified or not.
+            - 'title': the title of the review.
+            - 'userName': the user that created the review.
+    :return: A dataframe containing the reviews with the following columns: "datetime", "username",
+        "review", "rating", "reply", "reply_datetime", "thumbsup".
+    """
+    revs_cols = ["date", "userName", "review", "rating", "replyContent", "repliedAt"]
+    dataset_cols = ["Datetime", "Username", "Review", "Rating", "Reply", "Reply Datetime"]
+    # Preprocess results
+    for rev in revs:
+        # Concat title and review
+        rev["review"] = rev["title"] + rev["review"]
+        # Add response info
+        if "developerResponse" in rev:
+            rev["replyContent"] = rev["developerResponse"]["body"]
+            rev["repliedAt"] = datetime.strptime(rev["developerResponse"]["modified"], "%Y-%m-%dT%H:%M:%SZ")
+        else:
+            rev["replyContent"], rev["repliedAt"] = "", ""
+    return _format_generic(revs, dataset_cols, revs_cols)
+
+
+def format_playstore_reviews(
+        revs: list[dict]
+) -> pd.DataFrame:
+    """
+    Format the reviews obtained calling `retrieve_playstore_reviews` in a pandas dataframe.
+
+    :param revs: a list of dictionaries containing reviews from the Play Store.
+        Each review has the following parameter:
+            - 'reviewId': the id of the review.
+            - 'userName': the user that created the review.
+            - 'userImage': the image url of the user.
+            - 'content': the text of the review.
+            - 'score': the number of stars of the review (1-5).
+            - 'thumbsUpCount': the number of thumbs up.
+            - 'reviewCreatedVersion': the version of the app considered in the review.
+            - 'at': datetime of the review.
+            - 'replyContent': the response to the review.
+            - 'repliedAt': datetime of the response.
+            - 'appVersion': the current version of the app.
+    :return: A dataframe containing the reviews with the following columns: "datetime", "username",
+        "review", "rating", "reply", "reply_datetime", "thumbsup".
+    """
+    revs_cols = ["at", "userName", "content", "score", "replyContent", "repliedAt", "thumbsUpCount"]
+    dataset_cols = ["Datetime", "Username", "Review", "Rating", "Reply", "Reply Datetime", "Thumbs Up"]
+    return _format_generic(revs, dataset_cols, revs_cols)
+
+
+def _format_generic(revs, dataset_cols, revs_cols):
+    dataset = {key: [] for key in dataset_cols}
+    for rev in revs:
+        for rev_key, dataset_key in zip(revs_cols, dataset_cols):
+            # Remove all new line in string attributes to avoid problem when importing the csv in Excel
+            dataset[dataset_key].append(rev[rev_key].replace("\n", "") if isinstance(rev[rev_key], str)
+                                        else rev[rev_key])
+    return pd.DataFrame(dataset)
